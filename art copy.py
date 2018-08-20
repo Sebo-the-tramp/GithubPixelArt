7f97c32c27b2d88e0468f16c797b93ea0f296af8
Sebastian <sebastian.cavada.lab@gmail.com>
2018-08-20 20:19:19 +0100

    test

---
+++ art copy.py
@@ -0,0 +1,24 @@
+# make 10 random commits to github to a repo
+# with 3rd party key
+
+import os
+import random
+import time
+
+def main():
+    
+    # make 10 random commits
+    for i in range(10):
+        # make a random file
+        with open('random.txt', 'w') as f:
+            f.write(str(random.randint(0, 1000)))
+        # add file to git
+        os.system('git add random.txt')
+        # commit file
+        os.system('git commit -m "random commit"')
+        # push to github
+        os.system('git push')
+        # wait 1 second
+        time.sleep(1)
+
+main()
