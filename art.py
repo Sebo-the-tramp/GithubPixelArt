b22f2d3dcbff719fb58e2480c023e8876d885d14
Sebastian <sebastian.cavada.lab@gmail.com>
2022-10-04 18:32:40 +0200

    test

--- art.py
+++ art.py
@@ -1,23 +1,21 @@
-# make 10 random commits to github to a repo
+# commit to github repo with gitpython module
 
-import os
+from git import repo
 import random
-import time
+
+# commit once to github repo
 
 def main():
-    
-    # make 10 random commits
-    for i in range(10):
-        # make a random file
-        with open('random.txt', 'w') as f:
-            f.write(str(random.randint(0, 1000)))
-        # add file to git
-        os.system('git add random.txt')
-        # commit file
-        os.system('git commit -m "random commit" ' + str(time.time()))
-        # push to github
-        os.system('git push')
-        # wait 1 second
-        time.sleep(1)
+    # make a random file
+    with open('random.txt', 'w') as f:
+        f.write(str(random.randint(0, 1000)))
+    # add file to git
+    repo.index.add(['random.txt'])
+    # commit file
+    repo.index.commit('random commit')
+    # push to github
+    origin = repo.remote('origin')
+    origin.push()
+
 
 main()
